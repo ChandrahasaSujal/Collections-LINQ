b709fb8d697fa6df6b5d2181f4cf0769e647e76f
chanducodemonk <chanducodemonk@gmail.com>
2018-11-13 15:19:33 +0530

    Linq query on Lists and Array.

---
+++ Collections/LinqDemo3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class LinqDemo3
+    {
+        public static void Main()
+        {
+            List<int> l1 = new List<int>() { 13, 56, 98, 24, 62, 52, 83, 78, 39, 42, 91, 86, 6, 73, 67, 48, 18, 29, 3, 32, 15, 21, 2 };
+            var coll1 = from t in l1 where t < 40 orderby t select t;
+            Console.WriteLine(string.Join(" ", coll1));
+
+
+            int[] arr = { 13, 56, 98, 24, 62, 52, 83, 78, 39, 42, 91, 86, 6, 73, 67, 48, 18, 29, 3, 32, 15, 21, 2 };
+
+            var brr = from s in arr where s > 40 orderby s descending select s;
+
+            Console.WriteLine(string.Join(" ",brr));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Collections/LinqDemo4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class LinqDemo4
+    {
+        static void Main()
+        {
+            string[] colors = { "Red", "Blue", "Green", "White", "Black", "Yellow", "Pink", "Orange" };
+
+            // var str = from t in colors select t; //Accessing  all the colors from collection
+            //foreach(var s in str)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //var str = from t in colors where t.Length == 5 select t;   //Accessing colors having exact characters in the given collection.
+
+            //var str = from s in colors where s.StartsWith("R") select s;      // Accesing the elements of a collection whose elements starts  with R.
+
+            //var str = from s in colors where s.Substring(0,1) == "R" select s; // Accessing the elements of a collection starts with a letter R by making use of Substring mehtod.
+
+            //var str = from s in colors where s.EndsWith("e")  select s;   // Ends with letter e in a given collection.
+
+            var str = from s in colors where s.IndexOf("e") == -1 select s;
+
+            Console.WriteLine(string.Join(" ", str));
+            Console.ReadLine();
+        }
+    }
+}
